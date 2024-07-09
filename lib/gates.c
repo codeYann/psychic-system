e0192be5b6d23afbb7ca97dff1fac038a42c9495
Yan Avlis <codeyan10@gmail.com>
2024-07-09 09:33:10 -0300

    gate lib

---
+++ lib/gates.c
@@ -0,0 +1,7 @@
+#include "gates.h"
+
+int8_t and_gate(int8_t u, int8_t v) { return u & v; };
+
+int8_t or_gate(int8_t u, int8_t v) { return u | v; }
+
+int8_t not_gate(int8_t u) { return !u; }
---
+++ lib/gates.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+
+#ifndef GATES_H
+#define GATES_H
+
+int8_t and_gate(int8_t u, int8_t v);
+int8_t or_gate(int8_t u, int8_t v);
+int8_t not_gate(int8_t u);
+
+#endif // GATES_H
