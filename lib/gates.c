fb5e81571bb7bd875b928b12ff6ec17836f89c6b
Yan Avlis <codeyan10@gmail.com>
2024-07-11 09:33:24 -0300

    Adding xor gate

--- lib/gates.c
+++ lib/gates.c
@@ -5,3 +5,5 @@ int8_t and_gate(int8_t u, int8_t v) { return u & v; };
 int8_t or_gate(int8_t u, int8_t v) { return u | v; }
 
 int8_t not_gate(int8_t u) { return !u; }
+
+int8_t xor_gate(int8_t u, int8_t v) { return u ^ v; }
--- lib/gates.h
+++ lib/gates.h
@@ -6,5 +6,6 @@
 int8_t and_gate(int8_t u, int8_t v);
 int8_t or_gate(int8_t u, int8_t v);
 int8_t not_gate(int8_t u);
+int8_t xor_gate(int8_t u, int8_t v);
 
 #endif // GATES_H
