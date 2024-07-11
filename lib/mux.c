aabdb8e4ed5112e050d6d0eb325aa12be2b6d06c
Yan Avlis <codeyan10@gmail.com>
2024-07-11 09:33:36 -0300

    Adding a index verification

--- lib/mux.c
+++ lib/mux.c
@@ -3,5 +3,10 @@
 
 int8_t mux(int8_t inputs[8], int8_t select[3]) {
   int idx = (select[0] << 2) | (select[1] << 1) | select[2];
+
+  if (idx < 0b00000000 || idx > 0b00000111) {
+    return -1;
+  }
+
   return inputs[idx];
 }
