eac26c683d26f9234dee3fd63fc6cf653df88edc
Yan Avlis <codeyan10@gmail.com>
2024-07-11 10:16:17 -0300

    main.c

--- src/main.c
+++ src/main.c
@@ -1,17 +1,3 @@
-#include "lib/gate/gate.c"
-
-#include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char **argv) {
-
-  int8_t a = 1, b = 1;
-
-  Gate *gate = create(a, b);
-
-  int8_t result = and_operation(gate);
-  printf("and port => %d", result);
-
-  return 0;
-}
+int main(int argc, char **argv) { printf("Hello, World!"); }
