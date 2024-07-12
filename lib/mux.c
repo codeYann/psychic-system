c02b6da483a7a2f70e7d8625589dc707a6868022
Yan Avlis <codeyan10@gmail.com>
2024-07-12 09:34:49 -0300

    Using hexadecimal instead of using binary representation.

--- lib/mux.c
+++ lib/mux.c
@@ -4,7 +4,7 @@
 int8_t mux(int8_t inputs[8], int8_t select[3]) {
   int idx = (select[0] << 2) | (select[1] << 1) | select[2];
 
-  if (idx < 0b00000000 || idx > 0b00000111) {
+  if (idx < 0x00 || idx > 0x07) {
     return -1;
   }
 
